f577f681c27ed60822ff712d0686518396d25e1a
Zamo141 <dzamo141@gmail.com>
2020-03-28 15:39:32 -0700

    Its done

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require('express')
-const cowsay = require('cowsay')
 const cors = require('cors')
 const path = require('path')
 const user = require("./routes/user");
@@ -12,27 +11,6 @@ const app = express()
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'frontend/build')))
-
-// Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
-  try {
-    const text = req.params.say
-    const moo = cowsay.say({ text })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
-
-// Serve our base route that returns a Hello World cow
-app.get('/api/cow/', cors(), async (req, res, next) => {
-  try {
-    const moo = cowsay.say({ text: 'Hello World!' })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
