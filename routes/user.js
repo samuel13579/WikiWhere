49d50e10a9d6fa656007d10e0583f5722f77e2bf
samuel13579 <samuel32259@gmail.com>
2020-03-28 15:09:44 -0700

    added routes for test user

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require("/model/User");
+
+router.post('signup', async(req,res) => {
+    const {
+        username,
+        password,
+        email
+    } = req.body;
+
+
+    user = new User({
+        username,
+        password,
+        email
+    })
+
+    await user.save();
+})
