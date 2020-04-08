d5c53e2635fb8bf03940636cf96e404f8187a60b
samuel13579 <samuel32259@gmail.com>
2020-04-08 11:49:35 -0700

    changed how email tokens get sent and verified, for easier use

--- middleware/emailauth.js
+++ middleware/emailauth.js
@@ -2,13 +2,14 @@ const sgMail = require('@sendgrid/mail');
 
 module.exports = function(user, etoken) {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const link = `https://wiki-where.herokuapp.com/api/verify/${etoken}`
+    const link = `https://wiki-where.herokuapp.com/confirmation`
     const msg = {
         to: user.email,
         from: 'wikiwhere32816@gmail.com',
         subject: 'Email Authentication',
         text: 'Email Authentication',
-        html: `<p>Hi ${user.username}<p><br><p>Please click on the following <a href="${link}">link</a> to verify your account.</p> 
+        html: `<p>Hi ${user.username}<p><br><p>Please copy the token <br><br>${etoken}<br><br>
+        and enter at the following link <a href="${link}">link</a> to verify your account.</p> 
         <br><p>If you did not request this, please ignore this email.</p>`
     };
     sgMail.send(msg).then(() => {
