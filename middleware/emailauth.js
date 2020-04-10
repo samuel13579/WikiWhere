b08b3be099824905adde31754f7e6c2d121bc049
Zamo141 <dzamo141@gmail.com>
2020-04-10 16:23:39 -0700

    updated email message

--- middleware/emailauth.js
+++ middleware/emailauth.js
@@ -2,14 +2,13 @@ const sgMail = require('@sendgrid/mail');
 
 module.exports = function(user, etoken) {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const link = `https://wiki-where.herokuapp.com/confirmation`
+    const link = `https://wiki-where.herokuapp.com/verification/${etoken}`
     const msg = {
         to: user.email,
         from: 'wikiwhere32816@gmail.com',
         subject: 'Email Authentication',
         text: 'Email Authentication',
-        html: `<p>Hi ${user.username}<p><br><p>Please copy the token <br><br>${etoken}<br><br>
-        and enter at the following link <a href="${link}">link</a> to verify your account.</p> 
+        html: `<p>Hi ${user.username}<p><br><p>Click on the following <a href="${link}">link</a> to verify your account.</p> 
         <br><p>If you did not request this, please ignore this email.</p>`
     };
     sgMail.send(msg).then(() => {
