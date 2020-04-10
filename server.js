a88cb07debba46f8dd445ad59395f94d084e6b2b
Zamo141 <dzamo141@gmail.com>
2020-04-10 16:04:45 -0700

    cors bullshit fixed pog

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'frontend/build')))
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://wiki-where.herokuapp.com, *");
   res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, UPDATE, DELETE");
   next();
