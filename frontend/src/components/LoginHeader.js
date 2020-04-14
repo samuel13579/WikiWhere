a37c6fdd2e7c6ff6a5294d27ee3293ff0ab2a96a
masonproco <masonproco@knights.ucf.edu>
2020-04-14 15:50:57 -0400

    fix bug

--- frontend/src/components/LoginHeader.js
+++ frontend/src/components/LoginHeader.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Layout, Typography } from 'antd';
 import LoginCredentials from './LogInCredentials';
 import { withRouter } from 'react-router-dom';
+import logo from "./wikiwhere.png";
 
 const { Header } = Layout;
 const { Title } = Typography;
@@ -24,10 +25,12 @@ class LoginHeader extends Component {
     }
 
     render() {
+
         return(
             <Header className="site-layout-background" style={{height: 80}}>
                 <Title style={{ color: "white", position: 'absolute', marginTop: "25px"}}>
-                    <p style={{fontFamily: "Sans Serif", fontStyle: "Microsoft"}}>WikiWhere</p>
+                    <p style={{fontFamily: "Sans Serif", fontStyle: "Microsoft", marginRight:"200px"}}>WikiWhere</p>
+                    <img src={logo} style={{width:"100px", marginTop: "-200px", marginLeft: "75px"}}/>
                 </Title>
                 <LoginCredentials getCoords={this.getCoords} getToken={this.getToken}/>
             </Header>
